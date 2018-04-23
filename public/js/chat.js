9392214c1e2c1224a1fc1b6ebc9ac44812a70ee1
stizzle123 <arifayanidowu2@yahoo.com>
2018-04-24 00:54:30 +0100

    Send messages to only people in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -75,7 +75,6 @@ $('#message-form').on('submit', function(e) {
     e.preventDefault();
     let messageTextBox = $('[name=message]');
     socket.emit('createMessage', {
-        from: 'User',
         text: messageTextBox.val()
     }, function() {
         messageTextBox.val('');
